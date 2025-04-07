ffaccfb2cce679de861d9b39a9e8a8d0111f02ec
sumaruyadav <92817485+sumaruyadav@users.noreply.github.com>
2025-04-07 05:47:04 +0000

    htmlCssJsProject

---
+++ theColoursOfWeb/index.js
@@ -0,0 +1,30 @@
+document.getElementById("white").addEventListener("click", function(){
+    document.body.style.backgroundColor = "white";
+});
+document.getElementById("blue").addEventListener("click", function(){
+    document.body.style.backgroundColor = "blue";
+});
+document.getElementById("green").addEventListener("click", function(){
+    document.body.style.backgroundColor = "green";
+});
+document.getElementById("yellow").addEventListener("click", function(){
+    document.body.style.backgroundColor = "yellow";
+});
+document.getElementById("purple").addEventListener("click", function(){
+    document.body.style.backgroundColor = "purple";
+});
+document.getElementById("orange").addEventListener("click", function(){
+    document.body.style.backgroundColor = "orange";
+});
+document.getElementById("pink").addEventListener("click", function(){
+    document.body.style.backgroundColor = "pink";
+});
+document.getElementById("brown").addEventListener("click", function(){
+    document.body.style.backgroundColor = "brown";
+});
+document.getElementById("grey").addEventListener("click", function(){
+    document.body.style.backgroundColor = "grey";
+});
+document.getElementById("red").addEventListener("click", function(){
+    document.body.style.backgroundColor = "red";
+});
